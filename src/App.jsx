5e58249862ab0998a062c61057655ad17d19af9e
Chandan Kumar <chandankrr.91@gmail.com>
2024-09-22 20:03:42 +0530

    Setup tailwind css & font

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,9 @@
 const App = () => {
-  return <div>Hello World!</div>;
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    </div>
+  );
 };
 
 export default App;
